11e8dfda31b3ae9ee79f540841153234926357c1
Weihang Zheng <weihang.zheng@mail.utoronto.ca>
2023-09-17 13:38:22 -0400

    created email form and error checking for part 4

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, ValidationError, EmailField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -11,9 +11,9 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email Address?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,11 +21,25 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
+
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+
+        if 'utoronto' not in form.email.data.lower():
+            session['email_warning'] = 'Please use your UofT email.'
+        else:
+            session.pop('email_warning', None)
+
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), email_warning=session.get('email_warning'))
+
 
 @app.route('/user/<name>')
 def user(name):
